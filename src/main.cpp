b3543fcde08d08c1b000fc77a04425e3169e16ab
FractureFX <leg0rmail@gmail.com>
2025-12-09 08:14:04 -0500

    Pre-release commit #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <Geode/modify/PauseLayer.hpp>
+#include <Geode/modify/ProfilePage.hpp>
 
 using namespace geode::prelude;
 
 class $modify(MyPauseLayer, PauseLayer) {
 	void customSetup() {
 		PauseLayer::customSetup();
-		auto spr = CCSprite::createWithSpriteFrameName("GJ_infoBtn_001.png");
+		auto spr = CCSprite::createWithSpriteFrameName("GJ_profileButton_001.png");
+		spr->setScale(0.6f);
 		auto menu = this->getChildByID("left-button-menu");
 		auto btn = CCMenuItemSpriteExtra::create(
-			spr, this, nullptr
+			spr, this, menu_selector(MyPauseLayer::onProfileButton)
 		);
 
-		menu->addChild(btn);
-		spr->setScale(0.65f);
+		menu->addChild(btn, 0);
 		menu->updateLayout();
+	}
+
+	void onProfileButton(CCObject* sender) {
+		const auto gm = GJAccountManager::sharedState();
+		int accountID = gm->m_accountID;
+		const auto profileLayer = ProfilePage::create(accountID, true);
+		CCDirector::sharedDirector()->getRunningScene()->addChild(profileLayer, 100);
 	}
 };
